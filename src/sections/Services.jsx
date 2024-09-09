a9e8e524026ec42605039f48ead6ffa5845a9c43
Mohith Pranav <pranavmohith@gmail.com>
2024-09-09 19:05:09 +0530

    first commit

---
+++ src/sections/Services.jsx
@@ -0,0 +1,12 @@
+import { services } from "../constants";
+import ServicesCard from "../components/ServicesCard";
+
+export const Services = () => {
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service) => (
+        <ServicesCard key={service.label} {...service} />
+      ))}
+    </section>
+  );
+};
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import { Hero } from "./Hero";
+import { PopularProduct } from "./PopularProduct";
+import { SuperQuality } from "./SuperQuality";
+import { Services } from "./Services";
+import { SpecialOffer } from "./SpecialOffer";
+import { CustomerReview } from "./CustomerReview";
+import { Subscribe } from "./Subscribe";
+import { Footer } from "./Footer";
+
+export {
+  Hero,
+  PopularProduct,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReview,
+  Subscribe,
+  Footer,
+};
